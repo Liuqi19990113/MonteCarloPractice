ef7af76548667a0c81e6413ed1e4128979327714
Liuqi19990113 <78146233+Liuqi19990113@users.noreply.github.com>
2021-03-29 23:01:14 +0800

    Add files via upload

---
+++ sample_exp.py
@@ -0,0 +1,15 @@
+#  This program is used to sample exp distribution with MC method
+import random
+import math
+import matplotlib.pyplot as plt
+lamda = 3   # The pdf is lamba*exp(lamda*x)
+random_number = 1000000
+binbin = 500
+X_list = []
+for i in range(0, random_number):
+    X = -math.log(1-random.random())/lamda  #F^(-1)(Y)
+    X_list.append(X)
+plt.hist(X_list, binbin, color='g',density=True)
+plt.xlabel('X')
+plt.ylabel('probility/bin_step')
+plt.show()
